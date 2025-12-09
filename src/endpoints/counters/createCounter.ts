4c110734d9eb01209975f666d24c11f249135931
Krzysztof-StaCho <krzysztof.stachowaniec@gmail.com>
2025-12-09 19:39:23 +0100

    feat: add zod validation

--- src/endpoints/counters/createCounter.ts
+++ src/endpoints/counters/createCounter.ts
@@ -1,12 +1,17 @@
 import type { Request, Response } from "express";
 import { counterQueries } from "../../infrastructure/queries/counterQueries.js";
+import * as zod from "zod";
+
+const requestModel = zod.object({
+  name: zod.string()
+});
 
 export async function createCounter(req: Request, res: Response) {
-  const { name } = req.body;
-
-  if (!name) {
-    return res.status(400).json({ error: "Name is required" });
+  const result = requestModel.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json("Name is required");
   }
+  const { name } = result.data;
 
   try {
     const counter = await counterQueries.create(name);
