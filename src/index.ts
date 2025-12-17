d2263cf6d0f1c6e56e62ff2bec244bce85eca04e
ra-baal <balcerowskirafal@gmail.com>
2025-12-17 21:45:12 +0100

    chore: przywrócenie routera

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import { logoutAccount } from "./endpoints/accounts/logoutAccount.js";
 import { getInfoAccount } from "./endpoints/accounts/infoAccount.js";
 import countersRouter from "./endpoints/counters/countersRouter.js";
+import accountsRouter from "./endpoints/accounts/accountsRouter.js";
 
 const app = express();
 
@@ -48,11 +49,11 @@ app.get("/db-test", async (req, res) => {
   res.json({ version: version, time: time });
 });
 
-app.post("/account/register", registerAccount);
 app.post("/auth/login", loginAccount);
 app.get("/auth/logout", logoutAccount);
 app.get("/auth/me", getInfoAccount);
 
+app.use("/accounts", accountsRouter);
 app.use("/counters", countersRouter);
 
 // [server]
